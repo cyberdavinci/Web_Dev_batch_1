ddae1b2b302b478622b9e0a31bfbe10e8294d89e
cyberdavinci <ebrimaa2ray@gmail.com>
2023-09-19 04:53:24 -1200

    JS

---
+++ LandingPage/index.js
@@ -0,0 +1,33 @@
+// string
+let myName = "Ebrima";
+// float/decima
+const pi = 3.142;
+// number/ int
+let age = 34;
+// Arrays
+let data = ["ebrima", 3.142, 34];
+// objects
+const person = {
+  name: "Ebrima",
+  age: 10,
+  address: "Tipper Garage",
+  about: "A tech dude",
+};
+
+// ##################
+//  Operators
+//#################
+
+// String contcatenations
+let statement =
+  "My name is " + person.name + " and i am " + person.age + " years old ";
+//better way using string literals ``
+let statement2 = `My name is ${person.name} and i am ${person.age} years old`;
+// Addition
+
+let x = 10;
+let y = 4;
+let sum = x + y;
+
+// try substraction,multiplication and division on your own
+console.log(statement2);
