6496c0c1945d44f5b820f8e04b3528877a835b12
cyberdavinci <ebrimaa2ray@gmail.com>
2023-09-21 04:52:35 -1200

    update

--- LandingPage/index.js
+++ LandingPage/index.js
@@ -6,9 +6,9 @@ const pi = 3.142;
 // number/ int
 let age = 34;
 // Arrays
-let data = ["ebrima", 3.142, 34];
+let data = ["ebrima", 3.142, 34, ["solo", 232, true]];
 // objects
-let person = {
+const person = {
   name: "Ebrima",
   age: 10,
   address: "Tipper Garage",
@@ -18,7 +18,10 @@ let person = {
     phone: 12344343,
     email: "email@gmail.com",
   },
+  isMarried: false,
 };
+// Booleans
+// true and false
 
 // ##################
 //  Operators
@@ -27,7 +30,7 @@ let person = {
 // String contcatenations
 let statement =
   "My name is " + person.name + " and i am " + person.age + " years old ";
-//better way using string literals ``
+//better way using string literals/backticks ` `
 let statement2 = `My name is ${person.name} and i am ${person.age} years old`;
 // Addition
 
@@ -36,4 +39,12 @@ let y = 4;
 let sum = x + y;
 
 // try substraction,multiplication and division on your own
-console.log(data[1]);
+// using dot notation to get the values of object person
+console.log(person.about);
+// using scare notation to get the values of object person
+console.log(person["name"]);
+// using index/scare notation to get the values of datas array
+console.log(data[2]);
+
+console.log(statement);
+console.log(statement2);
